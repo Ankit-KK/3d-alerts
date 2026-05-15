2db8116b00da828a69f3a14503f57a3780105623
Ankit Kumar <ankitashuk20@gmail.com>
2026-05-15 15:04:48 +0530

    Replace HolographicRing with EmberRing in timeline

--- src/hooks/useDonationTimeline.ts
+++ src/hooks/useDonationTimeline.ts
@@ -5,7 +5,7 @@ import gsap from 'gsap';
 import * as THREE from 'three';
 import { useThree } from '@react-three/fiber';
 import { useDonationStore } from '@/store/useDonationStore';
-import type { HolographicRingHandle } from '@/components/HolographicRing';
+import type { EmberRingHandle } from '@/components/EmberRing';
 import { Howl } from 'howler';
 
 const sounds = {
@@ -18,7 +18,7 @@ function playSound(key: keyof typeof sounds) {
 }
 
 interface TimelineTargets {
-  ringRef: React.RefObject<HolographicRingHandle>;
+  ringRef: React.RefObject<EmberRingHandle>;
   nameRef: React.RefObject<THREE.Mesh>;
   amountRef: React.RefObject<THREE.Mesh>;
   messageRef: React.RefObject<THREE.Mesh>;
@@ -37,77 +37,52 @@ export function useDonationTimeline(targets: TimelineTargets) {
 
     tlRef.current?.kill();
     const tl = gsap.timeline({
-      onComplete: () => {
-        outro();
-      },
+      onComplete: () => outro(),
     });
     tlRef.current = tl;
 
     const ring = targets.ringRef.current;
 
-    // Intro sound
     tl.call(() => playSound('intro'), undefined, 0);
 
-    // Ring appear
     if (ring) {
       tl.to(ring.mesh!.scale, { x: 1, y: 1, z: 1, duration: 0.6, ease: 'back.out(1.7)' }, 0);
-
-      const color1Proxy = { r: 1, g: 0.4, b: 0 };
-      tl.to(color1Proxy, {
-        r: 1, g: 0.6, b: 0,
-        duration: 0.8,
-        onUpdate: () => {
-          if (ring.material) ring.material.uniforms.uColor1.value.setRGB(color1Proxy.r, color1Proxy.g, color1Proxy.b);
-        },
-      }, 0);
-
-      const color2Proxy = { r: 1, g: 0.8, b: 0 };
-      tl.to(color2Proxy, {
-        r: 1, g: 0.9, b: 0.2,
-        duration: 0.8,
-        onUpdate: () => {
-          if (ring.material) ring.material.uniforms.uColor2.value.setRGB(color2Proxy.r, color2Proxy.g, color2Proxy.b);
-        },
-      }, 0);
+      // Fire color transition
+      const c1 = { r: 1, g: 0.2, b: 0 };
+      tl.to(c1, { r: 1, g: 0.5, b: 0, duration: 0.8, onUpdate: () => {
+        if (ring.material) ring.material.uniforms.uColor1.value.setRGB(c1.r, c1.g, c1.b);
+      }}, 0);
+      const c2 = { r: 1, g: 0.7, b: 0 };
+      tl.to(c2, { r: 1, g: 0.9, b: 0.1, duration: 0.8, onUpdate: () => {
+        if (ring.material) ring.material.uniforms.uColor2.value.setRGB(c2.r, c2.g, c2.b);
+      }}, 0);
     }
 
-    // Text entrance
+    // Text scale animations
     if (targets.nameRef.current) {
-      tl.fromTo(
-        targets.nameRef.current.scale,
-        { x: 0, y: 0, z: 0 },
-        { x: 1, y: 1, z: 1, duration: 0.7, ease: 'elastic.out(1, 0.5)' },
-        '-=0.8'
-      );
+      tl.fromTo(targets.nameRef.current.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1, duration: 0.7, ease: 'elastic.out(1, 0.4)' }, '-=0.5');
     }
     if (targets.amountRef.current) {
-      tl.fromTo(
-        targets.amountRef.current.scale,
-        { x: 0, y: 0, z: 0 },
-        { x: 1, y: 1, z: 1, duration: 0.5, ease: 'back.out(2)' },
-        '-=0.4'
-      );
+      tl.fromTo(targets.amountRef.current.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1, duration: 0.5, ease: 'back.out(2.5)' }, '-=0.2');
     }
 
-    // Physics explosion & impact sound
+    // Physics explosion + sound
     tl.call(() => {
       targets.explodePhysics();
       playSound('impact');
-    }, undefined, '+=0.2');
+    }, undefined, '+=0.1');
 
     // Camera shake
-    const shakeProps = { x: 0, y: 0, z: 0 };
-    tl.to(shakeProps, {
-      x: 0.01, y: 0.01, z: 0.005,
-      duration: 0.08,
-      yoyo: true,
-      repeat: 5,
+    const shake = { x: 0, y: 0, z: 0 };
+    tl.to(shake, {
+      x: 0.015, y: 0.015, z: 0.005,
+      duration: 0.1, yoyo: true, repeat: 5,
       ease: 'power1.inOut',
       onUpdate: () => {
-        camera.rotation.x = shakeProps.x;
-        camera.rotation.y = shakeProps.y;
-        camera.rotation.z = shakeProps.z;
-      },
+        camera.rotation.x = shake.x;
+        camera.rotation.y = shake.y;
+        camera.rotation.z = shake.z;
+      }
     }, '-=0.1');
     tl.to(camera.rotation, { x: 0, y: 0, z: 0, duration: 0.3, ease: 'power2.out' }, '+=0.1');
 
@@ -131,8 +106,6 @@ export function useDonationTimeline(targets: TimelineTargets) {
       });
     };
 
-    return () => {
-      tl.kill();
-    };
+    return () => { tl.kill(); };
   }, [activeAlert]); // eslint-disable-line
 }
