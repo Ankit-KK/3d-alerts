ca4f8910243756778784744c51dc3550f7c84c1f
Ankit Kumar <ankitashuk20@gmail.com>
2026-05-15 14:15:07 +0530

    Add TextOverlay component for displaying donation alerts

---
+++ src/components/TextOverlay.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Text } from '@react-three/drei';
+import { useDonationStore } from '@/store/useDonationStore';
+import * as THREE from 'three';
+
+interface TextOverlayProps {
+  nameRef?: React.RefObject<THREE.Mesh>;
+  amountRef?: React.RefObject<THREE.Mesh>;
+  messageRef?: React.RefObject<THREE.Mesh>;
+}
+
+const TextOverlay = forwardRef<any, TextOverlayProps>(
+  ({ nameRef, amountRef, messageRef }, ref) => {
+    const activeAlert = useDonationStore((s) => s.activeAlert);
+    if (!activeAlert) return null;
+
+    const { donorName, amount, message, currency } = activeAlert;
+
+    return (
+      <group position={[0, 0.2, 0]}>
+        <Text
+          ref={nameRef}
+          fontSize={0.55}
+          color="#ffb86c"
+          anchorX="center"
+          anchorY="middle"
+          position={[0, 0.8, 0]}
+          outlineWidth={0.02}
+          outlineColor="#000000"
+          font="/fonts/Inter-Bold.woff"
+        >
+          {donorName}
+        </Text>
+        <Text
+          ref={amountRef}
+          fontSize={0.8}
+          color="#ffd000"
+          anchorX="center"
+          anchorY="middle"
+          position={[0, 0.1, 0]}
+          outlineWidth={0.04}
+          outlineColor="#000000"
+          font="/fonts/Inter-Bold.woff"
+        >
+          {currency}{amount}
+        </Text>
+        {message && (
+          <Text
+            ref={messageRef}
+            fontSize={0.3}
+            color="#ffffff"
+            anchorX="center"
+            anchorY="middle"
+            position={[0, -0.4, 0]}
+            font="/fonts/Inter-Regular.woff"
+          >
+            {message}
+          </Text>
+        )}
+      </group>
+    );
+  }
+);
+
+TextOverlay.displayName = 'TextOverlay';
+export { TextOverlay };
